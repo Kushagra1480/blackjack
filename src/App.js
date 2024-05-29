e6c6993c537f0d88b865d04dd2e0df752d17f52d
Kushagra1480 <kushagrakartik1480@gmail.com>
2024-05-29 13:49:26 -0700

    Added Player Score

--- src/App.js
+++ src/App.js
@@ -6,15 +6,18 @@ function App() {
   const [deck, setDeck] = useState([])
   const [playerHand, setPlayerHand] = useState([])
   const [dealerHand, setDealerHand] = useState([])
+  const [playerScore, setPlayerScore] = useState(0)
+  const [dealerScore, setDealerScore] = useState(0)
+  const [playerWin, setPlayerWin] = useState(false)
+  const [dealerWin, setDealerWin] = useState(false)
+
 
   const createDeck = () => {
     const suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
     const values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King']
-    
     const newDeck = suits.flatMap(suit => values.map(value => ({suit, value})))
     setDeck(newDeck)
   }
-
 
   const dealCards = () => {
     if (deck.length < 2) {
@@ -27,6 +30,37 @@ function App() {
     setDeck(shuffleDeck.slice(2))
     setPlayerHand(dealtCards)
     setDealerHand(dealerCards)
+    setPlayerScore(calculateScore(dealtCards))
+  }
+  const calculateScore = (hand) => {
+    let score = 0
+    let hasAce = false
+    for (const card of hand) {
+      switch(card.value) {
+        case 'Ace':
+          score += 11
+          hasAce = true
+          break
+        case 'King':
+        case 'Queen':
+        case 'Jack':
+          score += 10
+          break
+        default:
+          score += parseInt(card.value)
+      }
+    }
+    while (score > 21 && hasAce) {
+      score -= 10
+      hasAce = false
+    }
+    if (score === 21) {
+      setPlayerWin(true)
+    }
+    if (score > 21) {
+      setDealerWin(true)
+    }
+    return score 
   }
   React.useEffect(() => {
     createDeck();
@@ -39,11 +73,12 @@ function App() {
     const randomIndex = Math.floor(Math.random() * deck.length)
     const dealtCard = deck.splice(randomIndex, 1)[0]
     setPlayerHand([...playerHand, dealtCard])
+    setPlayerScore(calculateScore([...playerHand, dealtCard]))
   }
   return (
     <div className="App">
       {dealerHand.length == 0 && (
-        <button className="start-button" onClick={dealCards}>DEAL CARDS</button>
+        <button className="start-button" onClick={dealCards}>START GAME</button>
       )}
       {dealerHand.length > 0 && (
         <div className='game'>
@@ -58,6 +93,7 @@ function App() {
               <li key={index}>{card.value} of {card.suit}</li>
           ))}
         </ul>
+        <h4>Score: {playerScore}</h4>
         <div className='game-buttons'>
           <button onClick={hit}>Hit</button>
           <button>Stand</button>
