c4d5024ec2a71aa48bb30f3d8b2112c53a88b7ef
Kushagra1480 <kushagrakartik1480@gmail.com>
2024-06-06 15:06:31 -0700

    Fixed Styles of Back Card

--- src/App.js
+++ src/App.js
@@ -4,31 +4,79 @@ import axios from 'axios'
 
 function App() {
   const [game, setGame] = useState(false)
+  const [dealerTurn, setDealerTurn] = useState(false)
   const [deckID, setDeckID] = useState('')
   const [playerHand, setPlayerHand] = useState([])
   const [dealerHand, setDealerHand] = useState([])
-  
-  const getNewDeck = async () => {
-    try {
-      const response = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=4")
-      const newDeckID = response.data.deck_id
-      setDeckID(newDeckID)
-      console.log("Deck ID: ", newDeckID)
-      const playerHandResponse = await axios.get(`https://deckofcardsapi.com/api/deck/${newDeckID}/draw/?count=2`)
-      const newPlayerHand = playerHandResponse.data.cards
-      setPlayerHand(newPlayerHand)
-      const dealerHandResponse = await axios.get(`https://deckofcardsapi.com/api/deck/${newDeckID}/draw/?count=2`)
-      const newDealerHand = dealerHandResponse.data.cards
-      setDealerHand(newDealerHand)
-    } catch (e) {
-      console.error(e)
-    }
-  } 
+  const [playerScore, setPlayerScore] = useState(0)
+  const [dealerScore, setDealerScore] = useState(0)
 
+  const cardBack = "https://deckofcardsapi.com/static/img/back.png"
+  
+
+const fetchNewDeck = async () => {
+  try {
+    const { data: { deck_id: newDeckId } } = await axios.get(
+      "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=4"
+    );
+
+    setDeckID(newDeckId)
+
+    const { data: { cards: newPlayerHand } } = await axios.get(
+      `https://deckofcardsapi.com/api/deck/${newDeckId}/draw/?count=2`
+    );
+
+    setPlayerHand(newPlayerHand)
+    setPlayerScore(calculateScore(newPlayerHand))
+
+    const { data: { cards: newDealerHand } } = await axios.get(
+      `https://deckofcardsapi.com/api/deck/${newDeckId}/draw/?count=2`
+    );
+
+    setDealerHand(newDealerHand)
+
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+  const getCardPoints = (card) => {
+    let points = 0
+    switch(card.value) {
+      case 'Ace':
+        points = 11
+        break
+      case 'King':
+      case 'Queen':
+      case 'Jack':
+        points = 10
+        break
+      default:
+        points = parseInt(card.value)
+    }
+    return points
+  }
+
+  const calculateScore = (hand) => {
+    let score = 0
+    for(const card of hand) {
+      score = score + getCardPoints(card)
+    }
+    return score
+  }
   const playHelper = () => {
     setGame(true)
-    getNewDeck()
+    fetchNewDeck()
   }
+
+  const hitHelper = () => {
+    const { data: { cards: newPlayerHand } } = axios.get(
+      `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`
+    )
+    setPlayerHand([...playerHand, ...newPlayerHand])
+    setPlayerScore(calculateScore([...playerHand, ...newPlayerHand]))
+  }
+
 
   return (
     <div className="App">
@@ -40,20 +88,29 @@ function App() {
           <div className='hands'>
           <h2>Dealer's Hand:</h2>
             <div className='hand'>
-            {dealerHand.map((card, index) => 
+            {dealerTurn && dealerHand.map((card, index) => 
               <img src={card.images.svg} className='hand-card'/>
             )}
             </div>
+            {!dealerTurn && dealerHand.length > 0 && (
+              <div className='dealer-hand'> 
+                <img src={cardBack} className='hand-card back-card'/>
+                <img src={dealerHand[1].images.svg} className='hard-card other-card'/>
+              </div>
+            )}  
           <h2>Your Hand:</h2>
-          <div className='hand'>
+          <div className='hand '>
             {playerHand.map((card, index) => 
               <img src={card.images.svg} className='hand-card'/>
             )}
             </div>
           </div>
+          <div className='scores'>
+            <h3>Player Score: {playerScore}</h3>
+          </div>
           <div className='game-buttons'> 
-            <button className='game-button'>Hit</button>
-            <button className='game-button stand'>Stand</button>
+            <button className='game-button' onClick={hitHelper}>HIT</button>
+            <button className='game-button stand'>STAND</button>
           </div>
         </div>
       )}
